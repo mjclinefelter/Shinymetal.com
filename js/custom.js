1b6b52e1183d15e691cd1c0656d6a084d3627b16
mjclinefelter <mjclinefelter@gmail.com>
2017-05-18 17:40:37 -0500

    Initial Commit

---
+++ js/custom.js
@@ -0,0 +1,50 @@
+// --------------------------------------------------------
+// Pretty Photo for Lightbox Image
+// --------------------------------------------------------
+$(document).ready(function() {
+    $("a[data-gal^='prettyPhoto']").prettyPhoto();
+});
+
+// --------------------------------------------------------
+//	Navigation Bar
+// --------------------------------------------------------
+$(window).scroll(function(){
+	"use strict";
+	var scroll = $(window).scrollTop();
+	if( scroll > 60 ){
+		$(".navbar").addClass("scroll-fixed-navbar");
+	} else {
+		$(".navbar").removeClass("scroll-fixed-navbar");
+	}
+});
+
+// --------------------------------------------------------
+//	Smooth Scrolling
+// --------------------------------------------------------
+$(".navbar-nav li a[href^='#']").on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $(this.hash).offset().top
+    }, 1000);
+});
+
+
+/* form id="contactForm"
+
+
+window.addEventListener("DOMContentLoaded", function() {
+
+    var form = document.getElementById("contactForm");
+    document.getElementById("formSubmit").addEventListener("click", function () {
+      form.submit();
+      });
+
+  });
+*/
+
+// scroll effects
+window.sr = ScrollReveal();
+sr.reveal('#header-text');
+sr.reveal('.headline');
+sr.reveal('.features');
+sr.reveal('.portfolio-scroll');
---
+++ my.php
@@ -0,0 +1,9 @@
+<?php
+
+$name = $_GET['name'];
+$email = $_GET['email'];
+$phone = $_GET['phone'];
+$message = $_GET['message'];
+
+echo "welcome " .$name;
+?>
