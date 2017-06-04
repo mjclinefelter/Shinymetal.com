b50b5901641a484a22b7a775e7027f2225c5dffa
mjclinefelter <mjclinefelter@gmail.com>
2017-06-04 10:46:53 -0500

    Created Site new without excess plugin crap

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,4 @@
 // --------------------------------------------------------
-// Pretty Photo for Lightbox Image
-// --------------------------------------------------------
-$(document).ready(function() {
-    $("a[data-gal^='prettyPhoto']").prettyPhoto();
-});
-
-// --------------------------------------------------------
 //	Navigation Bar
 // --------------------------------------------------------
 $(window).scroll(function(){
@@ -24,27 +17,127 @@ $(window).scroll(function(){
 $(".navbar-nav li a[href^='#']").on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({
-        scrollTop: $(this.hash).offset().top - 70
+        scrollTop: $(this.hash).offset().top
     }, 1000);
 });
 
 
-/* form id="contactForm"
-
-
-window.addEventListener("DOMContentLoaded", function() {
-
-    var form = document.getElementById("contactForm");
-    document.getElementById("formSubmit").addEventListener("click", function () {
-      form.submit();
-      });
-
-  });
+/*
+|-------------------------------------------
+|	Simple Lightbox Video jQuery
+|-------------------------------------------
+|
+|
+|	Author: Pedro Marcelo
+|	Author URI: https://github.com/pedromarcelojava
+|	Plugin URI: https://github.com/pedromarcelojava/Simple-Lightbox-Video-jQuery
+|	Version: 2.0
 */
 
-// scroll effects
-window.sr = ScrollReveal();
-sr.reveal('#header-text');
-sr.reveal('.headline');
-sr.reveal('.features');
-sr.reveal('.portfolio-scroll');
+(function($)
+{
+	$.extend($.fn, {
+		simpleLightboxVideo : function()
+		{
+			var defaults = {
+				delayAnimation: 300,
+				keyCodeClose: 27
+			};
+
+			$.simpleLightboxVideo.vars = $.extend({}, defaults);
+
+			var video = this;
+
+			video.click(function()
+			{
+				if (window.innerHeight > 540)
+				{
+					var margintop = (window.innerHeight - 540) / 2;
+				}
+				else
+				{
+					var margintop = 0;
+				}
+
+				var ifr = '<iframe src="" width="640" height="480" id="slvj-video-embed" style="border:0;"></iframe>';
+				var close = '<div id="slvj-close-icon"></div>';
+				var lightbox = '<div class="slvj-lightbox" style="margin-top:' + margintop + 'px">';
+				var back = '<div id="slvj-back-lightbox">';
+				var bclo = '<div id="slvj-background-close"></div>';
+				var win = '<div id="slvj-window">';
+				var end = '</div></div></div>';
+
+				$('body').append(win + bclo + back + lightbox + close + ifr + end);
+				$('#slvj-window').hide();
+
+				if ($(this).data('videosite') == "youtube") {
+					var url = 'http://www.youtube.com/embed/' + $(this).data('videoid') + '?autoplay=1';
+				} else if($(this).data('videosite') == "vimeo") {
+					var url = 'http://player.vimeo.com/video/' + $(this).data('videoid') + '?autoplay=1';
+				}
+
+				$('#slvj-window').fadeIn();
+				$('#slvj-video-embed').attr('src', url);
+
+				$('#slvj-close-icon').click(function()
+				{
+					$('#slvj-window').fadeOut($.simpleLightboxVideo.vars.delayAnimation, function()
+					{
+						$(this).remove();
+					});
+				});
+
+				$('#slvj-background-close').click(function()
+				{
+					$('#slvj-window').fadeOut($.simpleLightboxVideo.vars.delayAnimation, function()
+					{
+						$(this).remove();
+					});
+				});
+
+				return false;
+			});
+
+			$(document).keyup(function(e)
+			{
+				if (e.keyCode == 27)
+				{
+					$('#slvj-window').fadeOut($.simpleLightboxVideo.vars.delayAnimation, function()
+					{
+						$(this).remove();
+					});
+				}
+			});
+
+			$(window).resize(function()
+			{
+				if (window.innerHeight > 540)
+				{
+					var margintop = (window.innerHeight - 540) / 2;
+				}
+				else
+				{
+					var margintop = 0;
+				}
+
+				$('.slvj-lightbox').css({
+					marginTop: margintop + 'px'
+				});
+			});
+
+			return false;
+		}
+	});
+})(jQuery);
+
+(function($)
+{
+	$.simpleLightboxVideo = function(options, video)
+	{
+		return $(video).simpleLightboxVideo();
+	}
+})(jQuery);
+
+
+
+$('.youtubevid a').simpleLightboxVideo();
--- my.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$name = $_GET['name'];
-$email = $_GET['email'];
-$phone = $_GET['phone'];
-$message = $_GET['message'];
-
-echo "welcome " .$name;
-?>
